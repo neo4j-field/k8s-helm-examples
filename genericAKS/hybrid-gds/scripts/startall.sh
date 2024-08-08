2cfa369f6cd463080bee13200cd6cdcd271193c0
Jeffrey Hair <jshair@yahoo.com>
2024-08-08 11:18:29 -0400

    Created and use variables at top of scripts

--- genericAKS/hybrid-gds/scripts/startall.sh
+++ genericAKS/hybrid-gds/scripts/startall.sh
@@ -1,42 +1,59 @@
 AKS_RESOURCE_GROUP=jhair-helm
 AKS_REGION=eastus
 
+AKS_CLUSTER_NAME=neo4j-aks-hybrid-cluster
+AKS_NAMESPACE=neo4j-hybrid
+AKS_NODEPOOL_NAME=neo4j
+
+#
 # Set the resource group in the defaults
+#
 az configure --defaults location=${AKS_REGION}
 az configure --defaults group=${AKS_RESOURCE_GROUP}
 
+#
 # List the defaults
+#
 echo "Azure default settings"
 az configure -l
 
+#
 # Create a resource group if it doesn't exist
+#
 if ! `az group exists --name ${AKS_RESOURCE_GROUP}`; then
    az group create --location ${AKS_REGION}--name ${AKS_RESOURCE_GROUP}
 else
    echo "Resource Group, ${AKS_RESOURCE_GROUP}, exists"
 fi
 
+#
 # Node count creates a 'system node pool'
+#
 echo "Creating AKS cluster..."
-az aks create --name neo4j-aks-hybrid-cluster --resource-group ${AKS_RESOURCE_GROUP}
+az aks create --name ${AKS_CLUSTER_NAME} --resource-group ${AKS_RESOURCE_GROUP}
 # --enable-oidc-issuer
 
 # Get the cluster node resource group
-MC_RG_NAME="$(az aks show --name cs-americas-aks-cluster --query nodeResourceGroup --output tsv)"
+MC_RG_NAME="$(az aks show --name ${AKS_CLUSTER_NAME} --query nodeResourceGroup --output tsv)"
 
-az aks get-credentials --overwrite-existing --name neo4j-aks-hybrid-cluster --admin
+az aks get-credentials --overwrite-existing --name ${AKS_CLUSTER_NAME} --admin
 
+#
+# Creating K8s namespace
+#
 echo "Creating namespace..."
-kubectl create namespace neo4j-hybrid
-kubectl config set-context --current --namespace=neo4j-hybrid
+kubectl create namespace ${AKS_NAMESPACE}
+kubectl config set-context --current --namespace=${AKS_NAMESPACE}
 
 kubectl create configmap license-config --from-file=../licenses/
 
-# Azure CLI to start nodepool
-#az aks nodepool start --resource-group myResourceGroup --cluster-name neo4j-aks-hybrid-cluster --nodepool-name neo4j_np
+# Azure CLI to start nodepool - separate nodepool from the 'system' nodepool
+# NOTE: The nodeSelector / nodegroup within the values yaml must match
+#       the nodegroup value within the label of the nodepool.
 echo "Creating nodepool..."
-az aks nodepool add --cluster-name neo4j-aks-hybrid-cluster \
-  --nodepool-name neo4j --node-count 4 \
+#az aks nodepool start --resource-group myResourceGroup --cluster-name ${AKS_CLUSTER_NAME} --nodepool-name neo4j_np
+az aks nodepool add --cluster-name ${AKS_CLUSTER_NAME} \
+  --nodepool-name ${AKS_NODEPOOL_NAME} --node-count 4 \
   --enable-cluster-autoscaler --min-count 1 --max-count 6 \
   --labels "nodegroup=playsmall" \
   --node-vm-size Standard_E16as_v5
@@ -45,15 +62,17 @@ echo "Sleeping 30 seconds to allow nodepool to start..."
 sleep 30
 
 echo "Creating Neo4j pods..."
-helm upgrade -i playsmall-1  neo4j/neo4j --namespace neo4j-hybrid -f hybrid-core-small.yaml 
+helm upgrade -i playsmall-1  neo4j/neo4j --namespace ${AKS_NAMESPACE} -f hybrid-core-small.yaml 
 sleep 5
-helm upgrade -i playsmall-2  neo4j/neo4j --namespace neo4j-hybrid -f hybrid-core-small.yaml
+helm upgrade -i playsmall-2  neo4j/neo4j --namespace ${AKS_NAMESPACE} -f hybrid-core-small.yaml
 sleep 5
-helm upgrade -i playsmall-3  neo4j/neo4j --namespace neo4j-hybrid -f hybrid-core-small.yaml
+helm upgrade -i playsmall-3  neo4j/neo4j --namespace ${AKS_NAMESPACE} -f hybrid-core-small.yaml
 sleep 5
-helm upgrade -i playsmall-gds-1  neo4j/neo4j --namespace neo4j-hybrid -f hybrid-gds-small.yaml 
+helm upgrade -i playsmall-gds-1  neo4j/neo4j --namespace ${AKS_NAMESPACE} -f hybrid-gds-small.yaml 
 
+#
 # Create load balancers
+#
 echo "Creating load balancers..."
 kubectl apply -f playsmall-lb.yaml
 kubectl apply -f playsmall-gds1-lb.yaml
@@ -71,6 +90,8 @@ kubectl apply -f playsmall-gds1-lb.yaml
     # --set jobSchedule="30 * * * *"
 #helm install jhair-backup neo4j/neo4j-admin -f backup-values.yaml
 
+echo "-------------------------------------------------------"
 kubectl get pods
+echo
 kubectl get services
 # kubectl run --rm -it --image "neo4j:5.16.0-enterprise" cypher-shell -- cypher-shell -a "neo4j://playsmall-3.default.svc.cluster.local:7687" -u neo4j -p "my-password"
--- genericAKS/hybrid-gds/scripts/stopall.sh
+++ genericAKS/hybrid-gds/scripts/stopall.sh
@@ -1,3 +1,10 @@
+AKS_RESOURCE_GROUP=jhair-helm
+AKS_REGION=eastus
+
+AKS_CLUSTER_NAME=neo4j-aks-hybrid-cluster
+AKS_NAMESPACE=neo4j-hybrid
+AKS_NODEPOOL_NAME=neo4j
+
 # List the defaults
 echo "Azure default settings"
 az configure -l
@@ -21,14 +28,14 @@ kubectl delete -f playsmall-gds1-lb.yaml --wait=false
 #kubectl get pods -o=name | awk '/playsmall-gds[0-9]-cleanup/{print $1}'| xargs kubectl delete -n efs 
 #sleep 30
 
-# Azure CLI to stop nodepool
+# Azure CLI to delete the nodepool
 echo "Deleting nodepool..."
-az aks nodepool delete --cluster-name neo4j-aks-hybrid-cluster  --nodepool-name neo4j --no-wait
+az aks nodepool delete --cluster-name ${AKS_CLUSTER_NAME}  --nodepool-name ${AKS_NODEPOOL_NAME} --no-wait
 
 #echo "Deleting AKS cluster (not waiting)..."
 #az aks delete --name neo4j-aks-hybrid-cluster --no-wait --yes
 echo "Deleting AKS cluster..."
-az aks delete --name neo4j-aks-hybrid-cluster --yes
+az aks delete --name ${AKS_CLUSTER_NAME} --yes
 
 #echo "The AKS cluster will be deleted in several minutes"
 
