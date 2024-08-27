14a8a4e6e5de4d94f8043e667227275dee8cf27d
Jeffrey Hair <jshair@yahoo.com>
2024-08-27 11:18:56 -0400

    Updated scripts, simplified standalone, etc

--- genericGKE/create-licenses-configmap.sh
+++ genericGKE/create-licenses-configmap.sh
@@ -1,3 +1,3 @@
-kubectl delete configmap license-config > /dev/null
+kubectl delete configmap license-config > /dev/null 2>&1 
 kubectl create configmap license-config --from-file=licenses/
 kubectl describe configmaps license-config
--- genericGKE/scripts/stopall.sh
+++ genericGKE/scripts/stopall.sh
@@ -1,5 +1,10 @@
+GKE_CLUSTER_NAME=jhair-gke-cluster
+
 echo "Deleting the LB..."
 kubectl delete -f standalone-lb.yaml
 
 echo "Uninstalling Neo4j from the GKE cluster..."
 helm uninstall standalone
+
+echo "Deleting the GKE cluster..."
+gcloud container clusters delete $GKE_CLUSTER_NAME 
